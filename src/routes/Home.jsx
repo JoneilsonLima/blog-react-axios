dd09191da3f33a6f83e91618556bbf9e6f66a2b0
joneilson pereira <joneilson84@gmail.com>
2024-02-12 17:12:54 -0300

    feat: busca de dados com axios

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,27 @@
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-export default Home
+import "./Home.css";
+
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async() => {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+      const data = response.data;
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+  
+  return <div>Home</div>;
+};
+
+export default Home;
