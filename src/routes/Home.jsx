4c131dedab4490d99830e491d1b6e2889fa66520
joneilson pereira <joneilson84@gmail.com>
2024-02-12 17:21:51 -0300

    feat: carregando lista de posts

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -9,8 +9,10 @@ const Home = () => {
 
   const getPosts = async() => {
     try {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
       const data = response.data;
+
+      setPosts(data);
     } catch (error) {
       console.log(error)
     }
@@ -21,7 +23,20 @@ const Home = () => {
     getPosts()
   }, [])
   
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Ultimos posts</h1>
+      {posts.length === 0 ? (<p>Carregando...</p>) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">Ler mais</Link>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Home;
